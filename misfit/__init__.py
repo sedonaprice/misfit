723a8dd163ff7d0ea68ccfdc3d0a9b6fea1eff81
Sedona Price <sedona.price@gmail.com>
2020-06-19 18:25:44 +0200

    Update __init__.py

--- misfit/__init__.py
+++ misfit/__init__.py
@@ -1,8 +1,8 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-"""
-This is an Astropy affiliated package.
-"""
+# """
+# This is an Astropy affiliated package.
+# """
 
 # Affiliated packages may add whatever they like to this file, but
 # should keep this content at the top.
