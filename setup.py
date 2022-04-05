7fda8064ada5accd945b50f302c2e8ec79f59729
Sedona Price <sedona.price@gmail.com>
2022-04-05 10:49:08 +0200

    Cleanup: not using astropy helpers, etc

--- setup.py
+++ setup.py
@@ -5,20 +5,8 @@ import glob
 import os
 import sys
 
-import ah_bootstrap
 from setuptools import setup
 
-# A dirty hack to get around some early import/configurations ambiguities
-if sys.version_info[0] >= 3:
-    import builtins
-else:
-    import __builtin__ as builtins
-builtins._ASTROPY_SETUP_ = True
-
-from astropy_helpers.setup_helpers import (register_commands, get_debug_option,
-                                           get_package_info)
-from astropy_helpers.git_helpers import get_git_devstr
-from astropy_helpers.version_helpers import generate_version_py
 
 # Get some values from the setup.cfg
 try:
@@ -65,24 +53,21 @@ else:
     package = sys.modules[PACKAGENAME]
     LONG_DESCRIPTION = package.__doc__
 
-# Store the package name in a built-in variable so it's easy
-# to get from other parts of the setup infrastructure
-builtins._ASTROPY_PACKAGE_NAME_ = PACKAGENAME
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
 
-if not RELEASE:
-    VERSION += get_git_devstr(False)
+# if not RELEASE:
+#     VERSION += get_git_devstr(False)
 
-# Populate the dict of setup command overrides; this should be done before
-# invoking any other functionality from distutils since it can potentially
-# modify distutils' behavior.
-cmdclassd = register_commands(PACKAGENAME, VERSION, RELEASE)
-
-# Freeze build information in version.py
-generate_version_py(PACKAGENAME, VERSION, RELEASE,
-                    get_debug_option(PACKAGENAME))
+# # Populate the dict of setup command overrides; this should be done before
+# # invoking any other functionality from distutils since it can potentially
+# # modify distutils' behavior.
+# cmdclassd = register_commands(PACKAGENAME, VERSION, RELEASE)
+#
+# # Freeze build information in version.py
+# generate_version_py(PACKAGENAME, VERSION, RELEASE,
+#                     get_debug_option(PACKAGENAME))
 
 # Treat everything in scripts except README* as a script to be installed
 scripts = [fname for fname in glob.glob(os.path.join('scripts', '*'))
@@ -133,7 +118,7 @@ setup(name=PACKAGENAME,
       license=LICENSE,
       url=URL,
       long_description=LONG_DESCRIPTION,
-      cmdclass=cmdclassd,
+      # cmdclass=cmdclassd,
       zip_safe=False,
       use_2to3=False,
       entry_points=entry_points,
